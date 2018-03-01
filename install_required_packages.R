505fab7979c47b8a8c72c75a31ef71612674c382
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2018-03-01 09:40:01 +0100

    updated install packages

--- install_required_packages.R
+++ install_required_packages.R
@@ -2,3 +2,5 @@ install.packages(c("devtools", "foreign", "Hmisc", "readxl",
                    "readstata13", "Epi", "car"))
 
 install.packages(c("knitr", "outliers", "mgcv", "MASS"))
+
+install.packages(c("rmarkdown", "compareGroups"))
