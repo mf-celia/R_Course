3d4c38958601dc7e5970a692dddeaaab0ec9a79c
Bioinformatic Research Group in Epidemiology (BRGE) <juanr.gonzalez@isglobal.org>
2018-11-27 09:36:01 +0100

    uploaded summary day 1

---
+++ Summary/R_basics.R
@@ -0,0 +1,41 @@
+# change working directory
+setwd("C:/Juan/CREAL/GitHub/R_course/Day1-Introduction/")
+
+# load data
+parto <- read.delim("data/parto2.dat", sep=";", header=FALSE)
+head(parto)
+View(parto)
+
+# create a new variable
+parto$newVar <- parto$V7/parto$V8
+head(parto)
+
+# create a new variable
+parto$newVar2 <- cut(parto$V7, c(-Inf, 20, 35, Inf),
+                     labels = c("bajo", "medio", "alto"))
+
+table(parto$newVar2)
+
+# quantiles
+parto$newVar3 <- cut(parto$V7, quantile(parto$V7),
+                     labels = c("1st", "2nd", "3th", "4h"))
+
+table(parto$newVar3)
+
+# subset (option 1)
+
+parto.sel <- subset(parto, parto$V9 == 2 & parto$V10 == 1)
+
+head(parto.sel)  
+
+# subset (option 2)
+
+sel <- parto$V9 == 2
+parto.sel2 <- parto[  sel ,  ]
+
+
+
+
+
+
+  
